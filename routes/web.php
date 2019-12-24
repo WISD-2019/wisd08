044f05b870dbad696cbd9c878fa0ab6aa572fa55
3A632011 <abc4173727@gmail.com>
2019-12-24 21:46:43 +0800

    aa.a2

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/aa/a1', function(){
     return view('aa.a1');
 });
+
+Route::get('/aa/a2', function(){
+    return view('aa.a2');
+});

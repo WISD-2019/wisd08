f406a3ee77e49b8276f62e5217985373e2a03310
3A632018 <f223145@gmail.com>
2019-12-26 00:21:17 +0800

    dd.d2

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,10 @@ Route ::get('/bb/b2',function(){
     return view('bb.b2');
 
 });
+Route ::get('/dd/d1',function(){
+    return view('dd.d1');
+
+});
 Route ::get('/dd/d2',function(){
     return view('dd.d2');
 
